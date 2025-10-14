c78e04ce99e423ec5c6aa645814bff7e6879e1c6
07mayyfranko <maylinfranco0705@gmail.com>
2025-10-14 18:11:30 -0500

    Add files via upload

---
+++ Ejercicio_1.java
@@ -0,0 +1,31 @@
+
+package secuenciales;
+
+/**
+ *
+ * @author Maylin Samara Franco Ruiz
+ */
+public class Ejercicio_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //crear variables
+        String username="Ginna";
+        int cofreUno=8;
+        int cofreDos=12;
+        int sumaDulces;
+        
+        //proceso
+        sumaDulces=cofreUno + cofreDos;
+        
+        //salida
+        System.out.print(username + " ahora tiene un total de " + sumaDulces + " caramelos mágicos.");
+       
+        
+    }
+    
+}
---
+++ Ejercicio_2.java
@@ -0,0 +1,33 @@
+
+package secuenciales;
+
+/**
+ *
+ * @author Maylin Samara Franco Ruiz
+ */
+public class Ejercicio_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        double primerReto=3.5;
+        double segundoReto=4.2;
+        double tercerReto=2.8;
+        double sumaRetos;
+        double divisionRetos;
+       
+        sumaRetos=(primerReto + segundoReto + tercerReto)/3;
+       
+        
+        System.out.print (" Tu promedio final es: " + sumaRetos + " ¡Nivel Superado! ");
+        
+        
+        
+        
+        
+    }
+    
+}
